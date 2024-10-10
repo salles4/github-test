442284f623418883a50b15be72d18dc0fa8607a1
salles4 <fjsalles.04@gmail.com>
2024-10-10 18:03:43 +0800

    fixed infinite loop

--- src/francis/PokemonDetails.jsx
+++ src/francis/PokemonDetails.jsx
@@ -1,16 +1,17 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function PokemonDetails() {
   const { name } = useParams();
 
   const [data, setData] = useState(null);
-
+useEffect(()=>{
   axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`).then((response) => {
     setData(response.data);
     console.log(response.data);
   });
+}, [name])
 
   return (
     <>
