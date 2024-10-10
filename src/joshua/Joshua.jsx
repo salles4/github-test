d0615a29a543b4763a61ac6425ee6d2685140054
jvimperial <joshuaimperial1010@gmail.com>
2024-10-10 08:46:20 +0800

    lahat pula

--- src/joshua/Joshua.jsx
+++ src/joshua/Joshua.jsx
@@ -1,8 +1,15 @@
+import './joshua.css';
 export default function Joshua() {
   return (
     <>
       <h1>Joshua</h1>
       <h1>LOGIN PAGE</h1>
+      <div>
+        <label>Username </label>
+        <input type="text" required placeholder="Username" name="username"></input>
+        <label>Password </label>
+        <input type="password" required placeholder="Password" name="userpass"></input>
+      </div>
     </>
   );
 }
